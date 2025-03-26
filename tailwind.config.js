682b064831f69ced0822a1d6f4ebcce38950d565
karimwo7 <kurbanovkarim61@gmail.com>
2025-03-26 14:47:29 +0500

    title

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
- /** @type {import('tailwindcss').Config} */
+/ @type {import('tailwindcss').Config} */
  export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src//*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+
+      'xs': '0px',
+      'md': '510px',
+      'lg': '1280px',
+
+  },
   },
   plugins: [],
 }
